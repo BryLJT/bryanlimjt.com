225185f63ff4ed167086fd065257fa3a77b36feb
Bryan Lim <bryanlimjt@gmail.com>
2026-05-13 12:01:45 +0800

    Add first 5 photos

--- data/photos.ts
+++ data/photos.ts
@@ -8,4 +8,10 @@ export type Photo = {
 export const categories = ["All", "Street", "Travel", "Nature", "People"] as const
 export type Category = typeof categories[number]
 
-export const photos: Photo[] = []
+export const photos: Photo[] = [
+  { src: "/images/photos/DSC_5597.JPG", alt: "test drive", caption: "test drive", category: "People" },
+  { src: "/images/photos/DSC_5610.JPG", alt: "home and simplicity", caption: "home and simplicity", category: "People" },
+  { src: "/images/photos/DSC_5714.JPG", alt: "kiss", caption: "kiss", category: "Nature" },
+  { src: "/images/photos/DSC_5742.JPG", alt: "zoooom", caption: "zoooom", category: "Street" },
+  { src: "/images/photos/DSC_5753.JPG", alt: "zooom 2", caption: "zooom 2", category: "Street" },
+]
